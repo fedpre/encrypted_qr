061d8464f2dd1ab68c061f0a893eac67622f1a24
fedpre <fed.pregnolato@gmail.com>
2023-01-10 13:59:52 +0100

    added files

---
+++ pages/index.js
@@ -0,0 +1,52 @@
+import CryptoJS from "crypto-js"
+import { useState } from 'react'
+import QRCode from "react-qr-code";
+
+const HomePage = () => {
+  const [cipherText, setCipherText] = useState('')
+  const [decipherText, setDecipherText] = useState()
+  const [string, setString] = useState('')
+  const [showQR, setShowQR] = useState(false)
+
+  let data = {"endpoint": "https://api.staging.moveshelf.com/graphql", "apiToken": "vRjfqotOT5e4T8T6wSRpnYY9txbe02fK1W7YXBZZyER6YxlQEUC6DcHeDA5_DGeJ", "monitoringType": "HealthKit", "ProjectName": "support/RPM_Demo", "PatientsName": "test_federico", "OrganizationName": "The Moveshelf Clinic"}
+
+  const encrypt = (data) => {
+    const encryptString = CryptoJS.AES.encrypt(JSON.stringify(data), 'no pain no gain').toString()
+    setCipherText(encryptString);
+  }
+
+  const decrypt = () => {
+    const bytes = CryptoJS.AES.decrypt(cipherText, 'no pain no gain');
+    const decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8))
+    setDecipherText(decryptedData.toString());
+  }
+
+  const generateQR = () => {
+
+  }
+  
+  return (
+    <>
+      <div>QR Code Generator</div>
+      <input type="text" onChange={(e) => setString(e.target.value)}></input>
+      <button onClick={() => encrypt(string)}>encrypt</button>
+      <p>{cipherText}</p>
+      <div>
+        <button onClick={decrypt}>decrypt</button>
+        <p>{decipherText}</p>
+      </div>
+      <button onClick={() => setShowQR(true)}>Generate QR Code</button>
+      <div style={{marginTop: "20px"}}>
+        {
+          showQR
+          ? <QRCode 
+            value={cipherText}
+          />
+          : ""
+        }
+      </div>
+    </>
+  )
+}
+
+export default HomePage
